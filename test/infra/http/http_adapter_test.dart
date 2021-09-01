0c1aa8ac9dcac2914340131037c319afc691d186
Leandro Rodrigues Souza <l.e.c.o321@hotmail.com>
2021-09-01 22:51:27 +0200

    ensure HttpAdapter calls post with correct Body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:faker/faker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart';
@@ -9,9 +11,9 @@ class HttpAdapter {
 
   HttpAdapter(this.client);
 
-  Future<void> request({@required Uri url, @required method}) async {
+  Future<void> request({@required Uri url, @required method, Map body}) async {
     final headers = {'content-type': 'application/json', 'accept': 'application/json'};
-    await client.post(url, headers: headers);
+    await client.post(url, headers: headers, body: jsonEncode(body));
   }
 }
 
@@ -34,9 +36,9 @@ void main() {
 
       var uri = Uri.parse(url);
 
-      await sut.request(url: uri, method: 'post');
+      await sut.request(url: uri, method: 'post', body: {'any_key': 'any_value'});
 
-      verify(client.post(uri, headers: {'content-type': 'application/json', 'accept': 'application/json'}));
+      verify(client.post(uri, headers: {'content-type': 'application/json', 'accept': 'application/json'}, body: '{"any_key":"any_value"}'));
       //verify(response);
     });
   });
