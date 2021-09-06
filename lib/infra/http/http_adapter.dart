3c264d14177f35519ede65ae6519d797feed6441
Leandro Rodrigues Souza <l.e.c.o321@hotmail.com>
2021-09-06 21:43:50 +0200

    move HttpAdapter to its own file

---
+++ lib/infra/http/http_adapter.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:fordev/data/http/http.dart';
+import 'package:http/http.dart';
+
+class HttpAdapter implements HttpClient {
+  final Client client;
+
+  HttpAdapter(this.client);
+
+  Future<Map> request({@required String url, @required method, Map body}) async {
+    
+    final headers = {'content-type': 'application/json', 'accept': 'application/json'};
+    final jsonBody = body != null ? jsonEncode(body) : body;
+    final response = await client.post(url, headers: headers, body: jsonBody);
+    if(response.statusCode == 200) {
+      return response.body.isEmpty ? null : jsonDecode(response.body);
+    } else {
+      return null;
+    }
+  }
+}
--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,29 +1,9 @@
-import 'dart:convert';
-
 import 'package:faker/faker.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:fordev/data/http/http_client.dart';
+import 'package:fordev/infra/http/http.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
-class HttpAdapter implements HttpClient {
-  final Client client;
-
-  HttpAdapter(this.client);
-
-  Future<Map> request({@required String url, @required method, Map body}) async {
-    
-    final headers = {'content-type': 'application/json', 'accept': 'application/json'};
-    final jsonBody = body != null ? jsonEncode(body) : body;
-    final response = await client.post(url, headers: headers, body: jsonBody);
-    if(response.statusCode == 200) {
-      return response.body.isEmpty ? null : jsonDecode(response.body);
-    } else {
-      return null;
-    }
-  }
-}
 
 class ClientSpy extends Mock implements Client {}
 
