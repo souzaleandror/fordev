5cadf1da41b456d9636bc2f4af56be3280054516
Leandro Rodrigues Souza <l.e.c.o321@hotmail.com>
2021-09-06 21:54:33 +0200

    ensure HttpAdapter throws Unauthorized 401

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -92,6 +92,14 @@ void main() {
       expect(future, throwsA(HttpError.badRequest));
     });
 
+    test('Should returns UnauthorizedError if post returns 4001', () async {
+      mockResponse(401);
+      
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.unauthorized));
+    });
+
     test('Should returns BadRequestError if post returns 500', () async {
       mockResponse(500);
       
