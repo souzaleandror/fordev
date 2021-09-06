1ff5b63698e6f3c0c863a594847c262992af022a
Leandro Rodrigues Souza <l.e.c.o321@hotmail.com>
2021-09-06 21:55:47 +0200

    ensure HttpAdapter throws Forbidden 403

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -92,12 +92,20 @@ void main() {
       expect(future, throwsA(HttpError.badRequest));
     });
 
-    test('Should returns UnauthorizedError if post returns 4001', () async {
+    test('Should returns UnauthorizedError if post returns 401', () async {
       mockResponse(401);
       
       final future = sut.request(url: url, method: 'post');
 
       expect(future, throwsA(HttpError.unauthorized));
+    });
+
+    test('Should returns UnauthorizedError if post returns 403', () async {
+      mockResponse(403);
+      
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.forbidden));
     });
 
     test('Should returns BadRequestError if post returns 500', () async {
