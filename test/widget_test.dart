efe4415342a60111e4419fa76890956f16084f31
Leandro Rodrigues Souza <l.e.c.o321@hotmail.com>
2021-09-20 22:29:17 +0200

    add app icon

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'file:///Users/leandrorodrigues/Desktop/fordev/assets/main.dart';
+import '../temp/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
