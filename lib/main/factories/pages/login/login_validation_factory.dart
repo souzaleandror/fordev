15bef61516698c8708f246363ddf777f577af7c9
Leandro Rodrigues Souza <l.e.c.o321@hotmail.com>
2021-09-30 00:11:47 +0200

    add ValidationBuilder

--- lib/main/factories/pages/login/login_validation_factory.dart
+++ lib/main/factories/pages/login/login_validation_factory.dart
@@ -1,3 +1,4 @@
+import 'package:fordev/main/builders/builders.dart';
 import 'package:fordev/ui/presentation/protocols/protocols.dart';
 import 'package:fordev/validation/protocols/protocols.dart';
 import 'package:fordev/validation/validators/validators.dart';
@@ -8,9 +9,7 @@ Validation makeLoginValidation() {
 
 List<FieldValidation> makeLoginValidations() {
   return [
-    RequiredFieldValidation('email'),
-    EmailValidation('email'),
-    RequiredFieldValidation('password'),
-    PasswordValidation('password'),
+    ...ValidationBuilder.field('email').required().email().build(),
+    ...ValidationBuilder.field('password').required().password().build(),
   ];
 }
