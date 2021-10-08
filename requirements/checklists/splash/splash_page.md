9cf456266619eeb76f2a341084e3517ec09820fc
Leandro Rodrigues Souza <l.e.c.o321@hotmail.com>
2021-10-08 22:54:01 +0200

    ensure SplashPage calls loadCurrentAccount on page load

--- requirements/checklists/splash/splash_page.md
+++ requirements/checklists/splash/splash_page.md
@@ -2,6 +2,6 @@
 
 > ## Regras
 1. ✅ Mostrar loading ao iniciar a pagina
-2. Chamar o carregar dados da conta atual
+2. ✅ Chamar o carregar dados da conta atual
 3. Navegar para a pagina correta ao final do carregamento
 4. Fechar streams quando a pagina for encerrada
--- test/ui/pages/splash_page_test.dart
+++ test/ui/pages/splash_page_test.dart
@@ -1,13 +1,17 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/route_manager.dart';
+import 'package:mockito/mockito.dart';
 import 'package:test/test.dart' as test;
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
-  const SplashPage({Key key}) : super(key: key);
+  const SplashPage({Key key, @required this.presenter}) : super(key: key);
+
+  final SplashPresenter presenter;
 
   @override
   Widget build(BuildContext context) {
+    presenter.loadCurrentAccount();
     return Scaffold(
       appBar: AppBar(
         title: Text('4Dev'),
@@ -19,15 +23,23 @@ class SplashPage extends StatelessWidget {
   }
 }
 
+abstract class SplashPresenter {
+  Future<void> loadCurrentAccount();
+}
+
+class SplashPresenterSpy extends Mock implements SplashPresenter {}
+
 void main() {
+  SplashPresenterSpy presenter;
   Future<void> loadPage(WidgetTester tester) async {
+    presenter = SplashPresenterSpy();
     return tester.pumpWidget(
       GetMaterialApp(
         initialRoute: '/',
         getPages: [
           GetPage(
             name: '/',
-            page: () => SplashPage(),
+            page: () => SplashPage(presenter: presenter),
           ),
         ],
       ),
@@ -37,6 +49,14 @@ void main() {
   testWidgets('Should presenter spinner on page load',
       (WidgetTester tester) async {
     await loadPage(tester);
+
     test.expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+
+  testWidgets('Should call loadCurrentAccount on page load',
+      (WidgetTester tester) async {
+    await loadPage(tester);
+
+    verify(presenter.loadCurrentAccount()).called(1);
   });
 }
