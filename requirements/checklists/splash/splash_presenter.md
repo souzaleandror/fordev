470d1ce1ab2cdf5b47fdb4e193968909ebb24b15
Leandro Rodrigues Souza <l.e.c.o321@hotmail.com>
2021-10-08 23:35:44 +0200

    ensure GetxSplashPresenter emits login page on empty results

--- requirements/checklists/splash/splash_presenter.md
+++ requirements/checklists/splash/splash_presenter.md
@@ -3,5 +3,5 @@
 > ## Regras
 1. ✅ Chamar o LoadCurrentAccount com a chave correta
 2. ✅ Levar o usuario para tela de Enquetes se tiver dados no cache
-3. Levar o usuario para tela de Login se nao tiver dados no cache
+3. ✅ Levar o usuario para tela de Login se nao tiver dados no cache
 4. Levar o usuario para tela de login se der erro ao carregar dados no cache
--- test/presentation/presenters/getx_splash_presenter_test.dart
+++ test/presentation/presenters/getx_splash_presenter_test.dart
@@ -1,4 +1,6 @@
+import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
+import 'package:fordev/domain/entities/account_entity.dart';
 import 'package:fordev/domain/usecases/load_current_account.dart';
 import 'package:fordev/ui/pages/pages.dart';
 
@@ -14,8 +16,8 @@ class GetxSplashPresenter implements SplashPresenter {
   Stream<String> get navigateToStream => _navigateTo.stream;
 
   Future<void> checkAccount() async {
-    await loadCurrentAccount.load();
-    _navigateTo.value = '/surveys';
+    final account = await loadCurrentAccount.load();
+    _navigateTo.value = account.isNull ? '/login' : '/surveys';
   }
 }
 
@@ -24,9 +26,15 @@ class LoadCurrentAccountSpy extends Mock implements LoadCurrentAccount {}
 void main() {
   LoadCurrentAccountSpy loadCurrentAccount;
   GetxSplashPresenter sut;
+
+  void mockLoadCurrentAccount({AccountEntity account}) {
+    when(loadCurrentAccount.load()).thenAnswer((_) async => account);
+  }
+
   setUp(() {
     loadCurrentAccount = LoadCurrentAccountSpy();
     sut = GetxSplashPresenter(loadCurrentAccount: loadCurrentAccount);
+    mockLoadCurrentAccount(account: AccountEntity(faker.guid.guid()));
   });
   test('should call LoadCurrentAccount', () async {
     await sut.checkAccount();
@@ -37,6 +45,12 @@ void main() {
   test('should go to surveys page on success', () async {
     sut.navigateToStream
         .listen(expectAsync1((page) => expect(page, '/surveys')));
+    await sut.checkAccount();
+  });
+
+  test('should go to login page on null result', () async {
+    mockLoadCurrentAccount(account: null);
+    sut.navigateToStream.listen(expectAsync1((page) => expect(page, '/login')));
     await sut.checkAccount();
   });
 }
