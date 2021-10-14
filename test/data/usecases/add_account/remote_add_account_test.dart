5dd988dcffa8ef61b9b1da354331522f2aba45e8
Leandro Rodrigues Souza <l.e.c.o321@hotmail.com>
2021-10-14 22:39:17 +0200

    Ensure RemoteAddAccount throws UnexpectedError on 200 with invalid data

--- test/data/usecases/add_account/remote_add_account_test.dart
+++ test/data/usecases/add_account/remote_add_account_test.dart
@@ -100,4 +100,14 @@ void main() {
 
     expect(account?.token, validData['accessToken']);
   });
+
+  test(
+      'Should throw UnexpectedError if HttpClient returns 200 with invalid data ',
+      () async {
+    mockHttpData({'invalid_key': 'invalid_value'});
+
+    final future = sut?.add(params); //action
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
