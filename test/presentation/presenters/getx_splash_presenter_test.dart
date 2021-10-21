8bb52cccba6bd2c7d1b533c23e8d3d6eab5ba7d5
Leandro Rodrigues Souza <l.e.c.o321@hotmail.com>
2021-10-21 23:37:16 +0200

    ensure SplashPresenter goes to login if token is null

--- test/presentation/presenters/getx_splash_presenter_test.dart
+++ test/presentation/presenters/getx_splash_presenter_test.dart
@@ -45,6 +45,12 @@ void main() {
     await sut.checkAccount(durationInSeconds: 0);
   });
 
+  test('should go to login page on null token', () async {
+    mockLoadCurrentAccount(account: AccountEntity(null));
+    sut.navigateToStream.listen(expectAsync1((page) => expect(page, '/login')));
+    await sut.checkAccount(durationInSeconds: 0);
+  });
+
   test('should go to login page on error', () async {
     mockLoadCurrentAccountError();
     sut.navigateToStream.listen(expectAsync1((page) => expect(page, '/login')));
