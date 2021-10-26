d57246d2663a99be868667dbd85d810fee5c1c82
Leandro Rodrigues Souza <l.e.c.o321@hotmail.com>
2021-10-26 21:48:02 +0200

    ensure HttpAdapter calls get with correct values

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -9,38 +9,42 @@ class HttpAdapter implements HttpClient {
 
   HttpAdapter(this.client);
 
-  Future<Map> request({@required String url, @required method, Map body}) async {
-    
-    final headers = {'content-type': 'application/json', 'accept': 'application/json'};
+  Future<Map> request(
+      {@required String url, @required method, Map body}) async {
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json'
+    };
     final jsonBody = body != null ? jsonEncode(body) : body;
     var response = Response('', 500);
     try {
-      if(method == 'post') {
+      if (method == 'post') {
         response = await client.post(url, headers: headers, body: jsonBody);
+      } else if (method == 'get') {
+        response = await client.get(url, headers: headers);
       }
-    } catch(error) {
+    } catch (error) {
       throw HttpError.serverError;
     }
-    
+
     return _handleResponse(response);
   }
 
   Map _handleResponse(Response response) {
-    if(response.statusCode == 200) {
+    if (response.statusCode == 200) {
       return response.body.isEmpty ? null : jsonDecode(response.body);
     } else if (response.statusCode == 204) {
       return null;
-    } else if(response.statusCode == 400) {
+    } else if (response.statusCode == 400) {
       throw HttpError.badRequest;
-    } else if(response.statusCode == 401) {
+    } else if (response.statusCode == 401) {
       throw HttpError.unauthorized;
-    } else if(response.statusCode == 403) {
+    } else if (response.statusCode == 403) {
       throw HttpError.forbidden;
-    } else if(response.statusCode == 404) {
+    } else if (response.statusCode == 404) {
       throw HttpError.notFound;
     } else {
       throw HttpError.serverError;
     }
   }
-
 }
