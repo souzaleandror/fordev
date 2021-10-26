cc575d2865305768a502cf64f32298bd4f013002
Leandro Rodrigues Souza <l.e.c.o321@hotmail.com>
2021-10-26 21:52:59 +0200

    ensure HttpAdapter throws  BadRequestError if get return 400 or with body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -200,5 +200,22 @@ void main() {
 
       expect(response, null);
     });
+
+    test('Should returns BadRequestError if get returns 400', () async {
+      mockResponse(400);
+
+      final future = sut.request(url: url, method: 'get');
+
+      expect(future, throwsA(HttpError.badRequest));
+    });
+
+    test('Should returns BadRequestError if get returns 400 with body empty',
+        () async {
+      mockResponse(400, body: '');
+
+      final future = sut.request(url: url, method: 'get');
+
+      expect(future, throwsA(HttpError.badRequest));
+    });
   });
 }
