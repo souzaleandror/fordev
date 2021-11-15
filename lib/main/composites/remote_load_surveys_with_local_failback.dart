2d1647ea37815a25ef1354639a2c6d86cd2b9448
Leandro Rodrigues Souza <l.e.c.o321@hotmail.com>
2021-11-15 23:26:47 +0100

    move RemoteLoadSurveysWithLocalFallback to its own file

---
+++ lib/main/composites/remote_load_surveys_with_local_failback.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:fordev/data/usecases/usecases.dart';
+import 'package:fordev/domain/entities/entities.dart';
+import 'package:fordev/domain/helpers/helpers.dart';
+import 'package:fordev/domain/usecases/usecases.dart';
+
+class RemoteLoadSurveysWithLocalFallback implements LoadSurveys {
+  final RemoteLoadSurveys remote;
+  final LocalLoadSurveys local;
+
+  RemoteLoadSurveysWithLocalFallback({
+    @required this.remote,
+    @required this.local,
+  });
+  Future<List<SurveyEntity>> load() async {
+    try {
+      final surveys = await remote.load();
+      await local.save(surveys);
+      return surveys;
+    } catch (error) {
+      if (error == DomainError.accessDenied) rethrow;
+      await local.validate();
+      return await local.load();
+    }
+  }
+}
