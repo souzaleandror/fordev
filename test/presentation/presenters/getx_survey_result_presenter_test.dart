a5721bf1674ef6f79f537d176bedcf6b7eb98a84
Leandro Rodrigues Souza <l.e.c.o321@hotmail.com>
2021-12-04 18:16:14 +0100

    ensure GetxSurveyResultPresenter emits correct events on success

--- test/presentation/presenters/getx_survey_result_presenter_test.dart
+++ test/presentation/presenters/getx_survey_result_presenter_test.dart
@@ -16,7 +16,8 @@ void main() {
   LoadSurveyResultSpy loadSurveyResult;
   SaveSurveyResultSpy saveSurveyResult;
   GetxSurveyResultPresenter sut;
-  SurveyResultEntity surveyResult;
+  SurveyResultEntity loadResult;
+  SurveyResultEntity saveResult;
   String surveyId;
   String answer;
 
@@ -42,8 +43,16 @@ void main() {
       when(loadSurveyResult.loadBySurvey(surveyId: anyNamed('surveyId')));
 
   void mockLoadSurveyResult(SurveyResultEntity data) {
-    surveyResult = data;
-    mockLoadSurveyResultCall().thenAnswer((_) async => surveyResult);
+    loadResult = data;
+    mockLoadSurveyResultCall().thenAnswer((_) async => loadResult);
+  }
+
+  PostExpectation mockSaveSurveyResultCall() =>
+      when(saveSurveyResult.save(answer: anyNamed('answer')));
+
+  void mockSaveSurveyResult(SurveyResultEntity data) {
+    saveResult = data;
+    mockSaveSurveyResultCall().thenAnswer((_) async => saveResult);
   }
 
   void mockLoadSurveyResultError() =>
@@ -63,6 +72,7 @@ void main() {
       surveyId: surveyId,
     );
     mockLoadSurveyResult(mockValidData());
+    mockSaveSurveyResult(mockValidData());
   });
 
   group('loadData', () {
@@ -79,19 +89,19 @@ void main() {
           (result) => expect(
             result,
             SurveyResultViewModel(
-              surveyId: surveyResult.surveyId,
-              question: surveyResult.question,
+              surveyId: loadResult.surveyId,
+              question: loadResult.question,
               answers: [
                 SurveyAnswerViewModel(
-                  image: surveyResult.answers[0].image,
-                  answer: surveyResult.answers[0].answer,
-                  isCurrentAnswer: surveyResult.answers[0].isCurrentAnswer,
-                  percent: '${surveyResult.answers[0].percent}%',
+                  image: loadResult.answers[0].image,
+                  answer: loadResult.answers[0].answer,
+                  isCurrentAnswer: loadResult.answers[0].isCurrentAnswer,
+                  percent: '${loadResult.answers[0].percent}%',
                 ),
                 SurveyAnswerViewModel(
-                  answer: surveyResult.answers[1].answer,
-                  isCurrentAnswer: surveyResult.answers[1].isCurrentAnswer,
-                  percent: '${surveyResult.answers[1].percent}%',
+                  answer: loadResult.answers[1].answer,
+                  isCurrentAnswer: loadResult.answers[1].isCurrentAnswer,
+                  percent: '${loadResult.answers[1].percent}%',
                 ),
               ],
             ),
@@ -124,6 +134,36 @@ void main() {
       await sut.save(answer: answer);
 
       verify(saveSurveyResult.save(answer: answer)).called(1);
+    });
+
+    test('Should emit correct events on success', () async {
+      expectLater(sut.isLoadingStream, emitsInOrder([true, false]));
+      sut.surveyResultStream.listen(
+        expectAsync1(
+          (result) => expect(
+            result,
+            SurveyResultViewModel(
+              surveyId: saveResult.surveyId,
+              question: saveResult.question,
+              answers: [
+                SurveyAnswerViewModel(
+                  image: saveResult.answers[0].image,
+                  answer: saveResult.answers[0].answer,
+                  isCurrentAnswer: saveResult.answers[0].isCurrentAnswer,
+                  percent: '${saveResult.answers[0].percent}%',
+                ),
+                SurveyAnswerViewModel(
+                  answer: saveResult.answers[1].answer,
+                  isCurrentAnswer: saveResult.answers[1].isCurrentAnswer,
+                  percent: '${saveResult.answers[1].percent}%',
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+
+      await sut.save(answer: answer);
     });
   });
 }
