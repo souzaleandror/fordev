e7fb24f309418369bce0b322cf9babc4bd064f36
Leandro Rodrigues Souza <l.e.c.o321@hotmail.com>
2021-12-04 18:23:41 +0100

    remove duplication GetxSurveyResultPresenter

--- lib/ui/presentation/presenters/getx_survey_result_presenter.dart
+++ lib/ui/presentation/presenters/getx_survey_result_presenter.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fordev/domain/entities/entities.dart';
 import 'package:get/get.dart';
 
 import '../../../ui/pages/survey_result/survey_result.dart';
@@ -25,41 +26,19 @@ class GetxSurveyResultPresenter extends GetxController
   });
 
   Future<void> loadData() async {
-    try {
-      isLoading = true;
-      final surveyResult =
-          await loadSurveyResult.loadBySurvey(surveyId: surveyId);
-      _surveyResult.value = SurveyResultViewModel(
-        surveyId: surveyResult.surveyId,
-        question: surveyResult.question,
-        answers: surveyResult.answers
-            .map(
-              (answer) => SurveyAnswerViewModel(
-                image: answer.image,
-                answer: answer.answer,
-                percent: '${answer.percent}%',
-                isCurrentAnswer: answer.isCurrentAnswer,
-              ),
-            )
-            .toList(),
-      );
-      isLoading = false;
-    } on DomainError catch (error) {
-      if (error == DomainError.accessDenied) {
-        isSessionExpired = true;
-      } else {
-        _surveyResult.subject.addError(UIError.unexpected.description);
-      }
-    } finally {
-      isLoading = false;
-    }
+    showResultOnAction(() => loadSurveyResult.loadBySurvey(surveyId: surveyId));
   }
 
   @override
   Future<void> save({@required String answer}) async {
+    showResultOnAction(() => saveSurveyResult.save(answer: answer));
+  }
+
+  @override
+  Future<void> showResultOnAction(Future<SurveyResultEntity> action()) async {
     try {
       isLoading = true;
-      final surveyResult = await saveSurveyResult.save(answer: answer);
+      final surveyResult = await action();
       _surveyResult.value = SurveyResultViewModel(
         surveyId: surveyResult.surveyId,
         question: surveyResult.question,
